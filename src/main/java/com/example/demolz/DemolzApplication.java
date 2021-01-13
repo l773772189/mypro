1b5e7e8ab829a72a3862b8d90b692a87aa6154f5
linzhe <773772189@qq.com>
2021-01-13 20:44:05 +0800

    初始化三个文件

---
+++ src/main/java/com/example/demolz/DemolzApplication.java
@@ -0,0 +1,13 @@
+package com.example.demolz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemolzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemolzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demolz/DemolzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demolz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemolzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
